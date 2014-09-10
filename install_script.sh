292e622cead5c686a8dd23bace8228eaef8f6a37
Sean Aubin <saubin@uwaterloo.ca>
2014-09-10 10:38:07 -0400

    Create install_script.sh

---
+++ install_script.sh
@@ -0,0 +1,7 @@
+cd /tmp
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+sudo apt-get install trash-cli
+sudo add-apt-repository ppa:kilian/f.lux
+sudo apt-get update
+sudo apt-get install fluxgui
