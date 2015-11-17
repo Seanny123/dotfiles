eff491cd742ef840cccbfabb843d5c7d9b748c01
Sean Aubin <saubin@uwaterloo.ca>
2015-11-17 14:10:30 -0500

    Update install_script.sh

--- install_script.sh
+++ install_script.sh
@@ -3,10 +3,11 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 sudo add-apt-repository ppa:kilian/f.lux
 sudo apt-get update
-sudo apt-get install fluxgui trash-cli xclip git
+sudo apt-get install --yes fluxgui trash-cli xclip git sublime-text
 sudo pip install ipython --upgrade
 sudo pip install ipdb
 sudo pip install flake8
 curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
 sudo apt-get install --yes nodejs
 sudo ln -s /usr/bin/nodejs /usr/bin/node
+wget https://www.rescuetime.com/installers/rescuetime_beta_amd64.deb
