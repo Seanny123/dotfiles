0a82a19fd0ff7d26785522935c1a498b0da0d6d4
Sean Aubin <saubin@uwaterloo.ca>
2015-11-17 14:07:23 -0500

    Update install_script.sh

--- install_script.sh
+++ install_script.sh
@@ -5,3 +5,8 @@ sudo add-apt-repository ppa:kilian/f.lux
 sudo apt-get update
 sudo apt-get install fluxgui trash-cli xclip git
 sudo pip install ipython --upgrade
+sudo pip install ipdb
+sudo pip install flake8
+curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
+sudo apt-get install --yes nodejs
+sudo ln -s /usr/bin/nodejs /usr/bin/node
