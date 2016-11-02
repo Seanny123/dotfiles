309a089677be2b446846f1f077dd3a996d614b51
Sean Aubin <saubin@uwaterloo.ca>
2016-11-02 13:34:26 +0900

    Update install_script.sh

--- install_script.sh
+++ install_script.sh
@@ -6,10 +6,11 @@ sudo dpkg -i google-chrome-stable_current_amd64.deb
 wget https://www.rescuetime.com/installers/rescuetime_beta_amd64.deb
 sudo dpkg -i rescuetime_beta_amd64.deb
 sudo add-apt-repository ppa:nathan-renniewaldock/flux
-sudo apt-get update
-sudo apt-get install --yes fluxgui trash-cli xclip git sublime-text
+sudo apt update
+sudo apt install --yes fluxgui trash-cli xclip git sublime-text
 
 # Python specific
+sudo apt install python-dev python-setuptools
+sudo pip -H install pip --upgrade
+sudo pip -H install ipython ipdb flake8 seaborn pandas tables
 sudo pip install jupyter --upgrade
-sudo pip install ipdb
-sudo pip install flake8
