2ebeabc74d74f91946a67351aefd34bed3876c3b
Sean Aubin <saubin@uwaterloo.ca>
2016-11-30 18:42:39 +0900

    Update install_script.sh

--- install_script.sh
+++ install_script.sh
@@ -12,9 +12,9 @@ sudo apt install --yes fluxgui trash-cli xclip git sublime-text filezilla gedit 
 # Python specific
 sudo apt install python-dev python-setuptools python-tk python-pip
 sudo apt install python3-dev python3-setuptools python3-tk python3-pip
-sudo pip -H install pip --upgrade
-sudo pip -H install ipython ipdb flake8 seaborn pandas tables
-sudo pip install jupyter --upgrade
+sudo -H pip install pip --upgrade
+sudo -H pip install ipython ipdb flake8 seaborn pandas tables
+sudo -H pip install jupyter --upgrade
 
 # VPN requirements
 sudo apt --yes install network-manager-openconnect lib32z1 lib32ncurses5
