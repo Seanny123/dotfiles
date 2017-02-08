bee1867abde4682c8273f491d26c436967d37c98
Sean Aubin <saubin@uwaterloo.ca>
2017-02-08 09:55:27 +0900

    Update install_script.sh

--- install_script.sh
+++ install_script.sh
@@ -7,7 +7,10 @@ wget https://www.rescuetime.com/installers/rescuetime_beta_amd64.deb
 sudo dpkg -i rescuetime_beta_amd64.deb
 sudo add-apt-repository ppa:nathan-renniewaldock/flux
 sudo apt update
-sudo apt install --yes fluxgui trash-cli xclip git sublime-text filezilla gedit unrar vlc pdfgrep
+sudo apt install --yes fluxgui trash-cli xclip git sublime-text filezilla gedit unrar vlc pdfgrep htop skype-bin
+sudo apt install --yes shutter
+
+# TODO: figure out how to automatically install newest version of Wine and FoxitReader Portable
 
 # Python specific
 sudo apt install python-dev python-setuptools python-tk python-pip
@@ -16,5 +19,7 @@ sudo -H pip install pip --upgrade
 sudo -H pip install ipython ipdb flake8 seaborn pandas tables pytest scipy scikit-learn
 sudo -H pip install jupyter --upgrade
 
-# VPN requirements
+# Waterloo VPN requirements
 sudo apt --yes install network-manager-openconnect lib32z1 lib32ncurses5
+
+# TODO: decide if I want to set up npm, typescript and Visual Studio Code on every machine I use
