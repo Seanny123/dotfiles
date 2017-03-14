26da5be313d1c07a738e4f6b4583d125c03f6085
Sean Aubin <saubin@uwaterloo.ca>
2017-03-14 18:45:28 +0900

    Update install_script.sh

--- install_script.sh
+++ install_script.sh
@@ -11,10 +11,11 @@ sudo apt install --yes fluxgui trash-cli xclip git sublime-text filezilla gedit 
 sudo apt install --yes shutter dropbox
 
 # TODO: figure out how to automatically install newest version of Wine and FoxitReader Portable
+# TODO: figure out how to install dropbox
 
 # Python specific
-sudo apt install python-dev python-setuptools python-tk python-pip
-sudo apt install python3-dev python3-setuptools python3-tk python3-pip
+sudo apt install --yes python-dev python-setuptools python-tk python-pip
+sudo apt install --yes python3-dev python3-setuptools python3-tk python3-pip
 sudo -H pip3 install pip3 --upgrade
 sudo -H pip3 install ipython ipdb flake8 seaborn pandas tables pytest scipy scikit-learn
 sudo -H pip3 install jupyter --upgrade
@@ -24,4 +25,5 @@ sudo apt --yes install network-manager-openconnect lib32z1 lib32ncurses5
 
 # TODO: decide if I want to set up npm, typescript and Visual Studio Code on every machine I use
 
-# TODO: remove the messaging and email stuff that I don't use that usually exists on a default Ubuntu install
+# Remove the games, messaging and email stuff that I don't use that usually exists on a default Ubuntu install
+sudo apt remove thunderbird aisleriot gnome-calendar gnome-mahjongg gnome-mines
