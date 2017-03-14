29cfd444b694cdd2bb73bb124bf5fa1f98c6cd70
Sean Aubin <saubin@uwaterloo.ca>
2017-03-14 20:17:09 +0900

    Update advanced_install.sh

--- advanced_install.sh
+++ advanced_install.sh
@@ -3,4 +3,4 @@ wget https://apps.ankiweb.net/downloads/current/anki_2.0.41_all.deb
 sudo dpkg -i anki_2.0.41_all.deb
 sudo apt -f install
 
-sudo apt install --yes inkscape bleachbit gummi gparted audacity
+sudo apt install --yes inkscape bleachbit gummi gparted audacity kdenlive
