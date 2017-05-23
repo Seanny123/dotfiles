1e1b1cbe0e8f2e5dea1cbb25fea2ecabea7a70f1
Sean Aubin <saubin@uwaterloo.ca>
2017-05-23 14:41:07 +0900

    Update windows_install.ps1

--- windows_install.ps1
+++ windows_install.ps1
@@ -3,14 +3,11 @@ Set-ExecutionPolicy AllSigned
 iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
 
 # Install utilities
-choco install --yes googlechrome firefox git notepadplusplus f.lux filezilla vlc sublimetext3 dropbox 7zip
-
-# Acquire Foxit Reader
-wget https://www.foxitsoftware.com/downloads/latest.php?product=Foxit-Reader&platform=Windows&package_type=exe&language=English -OutFile ~\Downdloads
+choco install --yes googlechrome firefox git notepadplusplus f.lux filezilla vlc sublimetext3 dropbox 7zip gpu-z foxitreader
 
 # JS dev installation
-choco install -yes visualstudiocode nodejs
+choco install -yes visualstudiocode nodejs yarn
 npm install -g tslint typescript
 
 # installing Python depends on how hardcore I want to be and thus is not included in this script until Numpy/Scipy
-# becomes compatible with OpenBLAS without having to use Conda
+# can be installed with OpenBLAS or MKL optimizations without having to use Conda
