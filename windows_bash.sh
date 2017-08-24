699519942be0c6de8040c052f321208db86334f3
Sean Aubin <saubin@uwaterloo.ca>
2017-08-24 19:05:26 -0400

    Create windows_bash.sh

---
+++ windows_bash.sh
@@ -0,0 +1,5 @@
+sudo apt install --yes trash-cli xclip git htop
+sudo apt install --yes python3-dev python3-setuptools python3-tk python3-pip
+sudo -H pip3 install pip --upgrade
+sudo -H pip3 install ipython ipdb flake8 seaborn pandas tables pytest scipy scikit-learn pylint notebook h5py hyperopt nbdime
+sudo -H pip3 install jupyter --upgrade
